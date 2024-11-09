8d0e379ec95a047a9818299e7481f1c9685c9d86
mgarbanzo <mgarbanzo@users.noreply.github.com>
2024-11-09 12:19:50 -0600

    Update 2_control_intensidad.py
    Comentarios

--- 2_control_intensidad.py
+++ 2_control_intensidad.py
@@ -3,4 +3,4 @@ from adafruit_circuitplayground import cp
 cp.pixels.brightness = 0.1
 
 while True:
-    cp.pixels[0] = (0, 0, 255)
+    cp.pixels[0] = (0, 0, 255) # Puede cambiar el primer 0 de 0 a 9 para mover la luz. Tambien puede cambias los ceros y el 255 para cambiar de color.
