6957974d811d1ee472737542f257b70bbe31dda3
jeanc1203 <69901653+jeanc1203@users.noreply.github.com>
2024-11-21 12:27:03 -0600

    Add files via upload

--- TBEAM/Pruebaloratsupreme/Pruebaloratsupreme.ino
+++ TBEAM/Pruebaloratsupreme/Pruebaloratsupreme.ino
@@ -1,7 +1,7 @@
 #include "LoRaBoards.h"
 #include <RadioLib.h>
 #include <TinyGPS++.h>
-// busca que reseptor lora se utiliza 
+
 #if     defined(USING_SX1276)
 #ifndef CONFIG_RADIO_FREQ
 #define CONFIG_RADIO_FREQ           868.0
@@ -110,19 +110,18 @@ LR1121 radio = new Module(RADIO_CS_PIN, RADIO_DIO9_PIN, RADIO_RST_PIN, RADIO_BUS
 TinyGPSPlus gps;
 #define MAX_LINE_LENGTH 21 // Maximum characters per line on the OLED
 //definicion de variables de estado
-// guarda el estado de la transmicion
+// save transmission state between loops
 static int transmissionState = RADIOLIB_ERR_NONE;
-// indicador de envio de paquete
+// flag to indicate that a packet was sent
 static volatile bool transmittedFlag = false;
-// variable que guarda el paquete a enviar
+static uint32_t counter = 0;
 static String datatrans;
-//variables para transmicion en el paquete
+//variables para transmicion
 String estadogps;
 String identificador="A";
 String numsat;
 String lat;
 String lon;
-// se define cada cuanto se envia un paquete
 int timeinit=millis();
 int timesend=1000;
 void setFlag(void)
@@ -168,19 +167,178 @@ void setup()
     }
 
     // set the function that will be called
-    // se define que funcion utilizar para verificar envio de dato
+    // when packet transmission is finished
     radio.setPacketSentAction(setFlag);
-// Se definen parametros para el envio de paketes 
-    radio.setFrequency(915.0);            // Frecuencia 
-    radio.setCodingRate(5);             // Coding Rate  (4/5)
-    radio.setSyncWord(0xAB);            // Sync Word 
-    radio.setCRC(false);                // CRC deshabilitado
+
+    /*
+    *   Sets carrier frequency.
+    *   SX1278/SX1276 : Allowed values range from 137.0 MHz to 525.0 MHz.
+    *   SX1268/SX1262 : Allowed values are in range from 150.0 to 960.0 MHz.
+    *   SX1280        : Allowed values are in range from 2400.0 to 2500.0 MHz.
+    *   LR1121        : Allowed values are in range from 150.0 to 960.0 MHz, 1900 - 2200 MHz and 2400 - 2500 MHz. Will also perform calibrations.
+    * * * */
+
+    if (radio.setFrequency(CONFIG_RADIO_FREQ) == RADIOLIB_ERR_INVALID_FREQUENCY) {
+        Serial.println(F("Selected frequency is invalid for this module!"));
+        while (true);
+    }
+
+    /*
+    *   Sets LoRa link bandwidth.
+    *   SX1278/SX1276 : Allowed values are 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125, 250 and 500 kHz. Only available in %LoRa mode.
+    *   SX1268/SX1262 : Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+    *   SX1280        : Allowed values are 203.125, 406.25, 812.5 and 1625.0 kHz.
+    *   LR1121        : Allowed values are 62.5, 125.0, 250.0 and 500.0 kHz.
+    * * * */
+    if (radio.setBandwidth(CONFIG_RADIO_BW) == RADIOLIB_ERR_INVALID_BANDWIDTH) {
+        Serial.println(F("Selected bandwidth is invalid for this module!"));
+        while (true);
+    }
+
+
+    /*
+    * Sets LoRa link spreading factor.
+    * SX1278/SX1276 :  Allowed values range from 6 to 12. Only available in LoRa mode.
+    * SX1262        :  Allowed values range from 5 to 12.
+    * SX1280        :  Allowed values range from 5 to 12.
+    * LR1121        :  Allowed values range from 5 to 12.
+    * * * */
+    if (radio.setSpreadingFactor(7) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR) {
+        Serial.println(F("Selected spreading factor is invalid for this module!"));
+        while (true);
+    }
+
+    /*
+    * Sets LoRa coding rate denominator.
+    * SX1278/SX1276/SX1268/SX1262 : Allowed values range from 5 to 8. Only available in LoRa mode.
+    * SX1280        :  Allowed values range from 5 to 8.
+    * LR1121        :  Allowed values range from 5 to 8.
+    * * * */
+    if (radio.setCodingRate(7) == RADIOLIB_ERR_INVALID_CODING_RATE) {
+        Serial.println(F("Selected coding rate is invalid for this module!"));
+        while (true);
+    }
+
+    /*
+    * Sets LoRa sync word.
+    * SX1278/SX1276/SX1268/SX1262/SX1280 : Sets LoRa sync word. Only available in LoRa mode.
+    * * */
+    if (radio.setSyncWord(0x12) != RADIOLIB_ERR_NONE) {
+        Serial.println(F("Unable to set sync word!"));
+        while (true);
+    }
+
+    /*
+    * Sets transmission output power.
+    * SX1278/SX1276 :  Allowed values range from -3 to 15 dBm (RFO pin) or +2 to +17 dBm (PA_BOOST pin). High power +20 dBm operation is also supported, on the PA_BOOST pin. Defaults to PA_BOOST.
+    * SX1262        :  Allowed values are in range from -9 to 22 dBm. This method is virtual to allow override from the SX1261 class.
+    * SX1268        :  Allowed values are in range from -9 to 22 dBm.
+    * SX1280        :  Allowed values are in range from -18 to 13 dBm. PA Version range : -18 ~ 3dBm
+    * LR1121        :  Allowed values are in range from -17 to 22 dBm (high-power PA) or -18 to 13 dBm (High-frequency PA)
+    * * * */
+    if (radio.setOutputPower(CONFIG_RADIO_OUTPUT_POWER) == RADIOLIB_ERR_INVALID_OUTPUT_POWER) {
+        Serial.println(F("Selected output power is invalid for this module!"));
+        while (true);
+    }
+
+#if !defined(USING_SX1280) && !defined(USING_LR1121) && !defined(USING_SX1280PA)
+    /*
+    * Sets current limit for over current protection at transmitter amplifier.
+    * SX1278/SX1276 : Allowed values range from 45 to 120 mA in 5 mA steps and 120 to 240 mA in 10 mA steps.
+    * SX1262/SX1268 : Allowed values range from 45 to 120 mA in 2.5 mA steps and 120 to 240 mA in 10 mA steps.
+    * NOTE: set value to 0 to disable overcurrent protection
+    * * * */
+    if (radio.setCurrentLimit(140) == RADIOLIB_ERR_INVALID_CURRENT_LIMIT) {
+        Serial.println(F("Selected current limit is invalid for this module!"));
+        while (true);
+    }
+#endif
+
+    /*
+    * Sets preamble length for LoRa or FSK modem.
+    * SX1278/SX1276 : Allowed values range from 6 to 65535 in %LoRa mode or 0 to 65535 in FSK mode.
+    * SX1262/SX1268 : Allowed values range from 1 to 65535.
+    * SX1280        : Allowed values range from 1 to 65535. preamble length is multiple of 4
+    * LR1121        : Allowed values range from 1 to 65535.
+    * * */
+    if (radio.setPreambleLength(16) == RADIOLIB_ERR_INVALID_PREAMBLE_LENGTH) {
+        Serial.println(F("Selected preamble length is invalid for this module!"));
+        while (true);
+    }
+
+    // Enables or disables CRC check of received packets.
+    if (radio.setCRC(true) == RADIOLIB_ERR_INVALID_CRC_CONFIGURATION) {
+        Serial.println(F("Selected CRC is invalid for this module!"));
+        while (true);
+    }
+
+#if  defined(USING_LR1121)
+    // LR1121
+    // set RF switch configuration for Wio WM1110
+    // Wio WM1110 uses DIO5 and DIO6 for RF switching
+    static const uint32_t rfswitch_dio_pins[] = {
+        RADIOLIB_LR11X0_DIO5, RADIOLIB_LR11X0_DIO6,
+        RADIOLIB_NC, RADIOLIB_NC, RADIOLIB_NC
+    };
+
+    static const Module::RfSwitchMode_t rfswitch_table[] = {
+        // mode                  DIO5  DIO6
+        { LR11x0::MODE_STBY,   { LOW,  LOW  } },
+        { LR11x0::MODE_RX,     { HIGH, LOW  } },
+        { LR11x0::MODE_TX,     { LOW,  HIGH } },
+        { LR11x0::MODE_TX_HP,  { LOW,  HIGH } },
+        { LR11x0::MODE_TX_HF,  { LOW,  LOW  } },
+        { LR11x0::MODE_GNSS,   { LOW,  LOW  } },
+        { LR11x0::MODE_WIFI,   { LOW,  LOW  } },
+        END_OF_MODE_TABLE,
+    };
+    radio.setRfSwitchTable(rfswitch_dio_pins, rfswitch_table);
+
+    // LR1121 TCXO Voltage 2.85~3.15V
+    radio.setTCXO(3.0);
+#endif
+
+#ifdef USING_DIO2_AS_RF_SWITCH
+#ifdef USING_SX1262
+    // Some SX126x modules use DIO2 as RF switch. To enable
+    // this feature, the following method can be used.
+    // NOTE: As long as DIO2 is configured to control RF switch,
+    //       it can't be used as interrupt pin!
+    if (radio.setDio2AsRfSwitch() != RADIOLIB_ERR_NONE) {
+        Serial.println(F("Failed to set DIO2 as RF switch!"));
+        while (true);
+    }
+#endif //USING_SX1262
+#endif //USING_DIO2_AS_RF_SWITCH
+
+#ifdef RADIO_RX_PIN
+    // SX1280 PA Version
+    radio.setRfSwitchPins(RADIO_RX_PIN, RADIO_TX_PIN);
+#endif
+
+#ifdef RADIO_SWITCH_PIN
+    // T-MOTION
+    const uint32_t pins[] = {
+        RADIO_SWITCH_PIN, RADIO_SWITCH_PIN, RADIOLIB_NC,
+    };
+    static const Module::RfSwitchMode_t table[] = {
+        {Module::MODE_IDLE,  {0,  0} },
+        {Module::MODE_RX,    {1, 0} },
+        {Module::MODE_TX,    {0, 1} },
+        END_OF_MODE_TABLE,
+    };
+    radio.setRfSwitchTable(pins, table);
+#endif
+
+
+
+
     // start transmitting the first packet
-    //Serial.print(F("Radio Sending first packet ... "));
+    Serial.print(F("Radio Sending first packet ... "));
 
     // you can transmit C-string or Arduino string up to
     // 256 characters long
-    //transmissionState = radio.startTransmit(String(counter).c_str());
+    transmissionState = radio.startTransmit(String(counter).c_str());
 
     // you can also transmit byte array up to 256 bytes long
     /*
@@ -191,14 +349,17 @@ void setup()
     delay(1000);
 // apartado de preparacion pantalla
 
-// imprime en pantalla el estado del reseptor lora y un mesage de busqueda de gps 
+
     u8g2->clearBuffer();
     u8g2->setFont(u8g2_font_5x8_tr); // Set a compact font for better text fitting
     u8g2->drawStr(0, 10, String("Lora init "+lora).c_str());
-     u8g2->sendBuffer();
-    u8g2->drawStr(5, 20, "Initializing GPS...");
+    u8g2->drawStr(0, 20, String("BW "+String(CONFIG_RADIO_BW)).c_str());
+    u8g2->drawStr(0, 30, String("FREQ "+String(CONFIG_RADIO_FREQ)).c_str());
+    u8g2->drawStr(0, 40, String("SpreadingFactor "+String(7)).c_str());
+    u8g2->drawStr(0, 50, String("SyncWord "+String("0x12")).c_str());
+    //u8g2->drawStr(5, 20, "Initializing GPS...");
     u8g2->sendBuffer();
-
+delay(20000);
     Serial.println(F("GPS OLED Example"));
     Serial.println(F("Displaying GPS data on SH1106 OLED"));
 }
@@ -243,34 +404,32 @@ void loop()
 
         u8g2->drawStr(0, 40, altStr);
         u8g2->drawStr(0, 50, speedStr);
-        // se prepara el mensage que se va a mandar en formato identificador,estado del gps,numero de satelites, latitud, longitud
-        datatrans= identificador + ","+estadogps+","+numsat+","+lat+","+lon;
-        // envia datos al buffer del display 
+        datatrans= String(0)+","+String(0)+","+identificador + ","+estadogps+","+numsat+","+lat+","+lon;
         u8g2->sendBuffer();
     }
     else
     {
       estadogps=String(0);
         // Display a message if no valid GPS data is available
-        datatrans= identificador + ","+estadogps;
+        datatrans= String(0)+","+String(0)+","+identificador + ","+estadogps;
         u8g2->drawStr(0, 10, "Waiting for GPS2...");
          u8g2->sendBuffer();
     }
     smartDelay(1000);
-  // envia datos por lora 
+
   if (millis()-timeinit>timesend){
     timeinit=millis();
     if (transmittedFlag) {
 
 
-        // reinicia la variable identificadora de envio de senal, si esta es true es porque ya se envio un pakete
+        // reset flag
         transmittedFlag = false;
 
         flashLed();
 
 
         if (transmissionState == RADIOLIB_ERR_NONE) {
-            // mensages de que se envio el dato
+            // packet was successfully sent
             Serial.println(F("transmission finished!"));
             u8g2->drawStr(5, 60, "TF");
             u8g2->sendBuffer();
