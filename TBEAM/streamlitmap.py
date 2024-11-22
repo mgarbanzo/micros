dd173ce505c646b2f65d3eaeedebe10be7d125bd
jeanc1203 <69901653+jeanc1203@users.noreply.github.com>
2024-11-21 21:04:19 -0600

    Update streamlitmap.py
    se agrega un cuadrado alrededor de la zona con una marca de una estrella

--- TBEAM/streamlitmap.py
+++ TBEAM/streamlitmap.py
@@ -13,18 +13,37 @@ positions = [
 ]
 
 # Crear el mapa centrado en la primera posición
-m = folium.Map(location=positions[0], zoom_start=8)
-
+m = folium.Map(location=(9.834066260020085, -83.91726983862638), zoom_start=11)
+latc=9.834066260020085
+lonc=-83.91726983862638
+n=0.2
+square_coordinates = [
+    [latc - n, lonc - n],
+    [latc - n, lonc + n],
+    [latc + n, lonc + n],
+    [latc + n, lonc - n],
+    [latc - n, lonc - n]  # Volver al punto inicial para cerrar el polígono
+]
 # Dibujar líneas entre las posiciones
 folium.PolyLine(positions, color="blue", weight=2.5, opacity=1).add_to(m)
+ls = folium.PolyLine(
+    locations=square_coordinates, color="red"
+)
+folium.Marker(
+    location=[latc, lonc],
+    popup="Centro",
+    icon=folium.Icon(icon='star', prefix='fa', color='blue')
+).add_to(m)
 
+ls.add_child(folium.Popup("limites hacienda"))
+ls.add_to(m)
 # Agregar marcadores para las posiciones
 for i, pos in enumerate(positions):
     if i == len(positions) - 1:
         # Último punto con letra roja
         folium.Marker(
             location=pos,
-            icon=folium.DivIcon(html=f"""<div style="font-size: 16px; color: red;">{identi>
+            icon=folium.DivIcon(html=f"""<div style="font-size: 16px; color: red;">{identificador}</div>""")
         ).add_to(m)
     else:
         # Puntos azules
@@ -39,3 +58,6 @@ for i, pos in enumerate(positions):
 
 # Crear columnas para expandir el mapa a toda la pantalla
 st.title("Mapa de Costa Rica con trayecto")
+    # Mostrar el mapa ajustado
+st_folium(m, width=2200, height=800)  # Ajusta el tamaño del mapa
+
